f11d9013c48421b7c5fe5aa3a51d50a99ee362ae
diongeorge <diongeorge@gmail.com>
2017-10-08 02:53:19 +0530

    Getting the list of all the folders in the directory

--- toDisorganize.py
+++ toDisorganize.py
@@ -1,3 +1,11 @@
 import os
+import shutil
 import os.path
+import numpy as np
+
 currentPath = os.getcwd()
+dirs = [d for d in os.listdir() if os.path.isdir(os.path.join( d))]
+print (dirs)
+slash = "\\"
+
+listToArray = np.asarray(dirs)
