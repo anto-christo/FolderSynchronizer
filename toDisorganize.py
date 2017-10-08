fde8a5fc622342cd612e1aefe0060b95980c9e19
Anto <anto.christo.20@gmail.com>
2017-10-08 15:16:17 +0530

    Remove numpy dependence

--- toDisorganize.py
+++ toDisorganize.py
@@ -1,16 +1,13 @@
 import os
 import shutil
 import os.path
-import numpy as np
 
 currentPath = os.getcwd()
 dirs = [d for d in os.listdir() if os.path.isdir(os.path.join( d))]
 print (dirs)
 slash = "\\"
 
-listToArray = np.asarray(dirs)
-
-for x in listToArray:
+for x in dirs:
     oldpath = currentPath + slash + x
     print(oldpath)
     directoryArray = os.listdir(x)
